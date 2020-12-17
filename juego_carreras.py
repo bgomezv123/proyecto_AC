11df9d5f7ce9f71892f465cefd67381ff40660da
bgomezv123 <bgomezv@unsa.edu.pe>
2020-12-16 22:01:07 -0500

    Se establecio la cordenada X cambiante por el objeto que catura la camra como variable global para controlar la posicion del carro

--- juego_carreras.py
+++ juego_carreras.py
@@ -29,41 +29,54 @@ cap = cv2.VideoCapture(0)
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 
-
-
 def pointCoordenates(frame):
-    verdeBajo = np.array([36, 100, 20])
-    verdeAlto = np.array([70, 255, 255])    
-    frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(frameHSV, verdeBajo, verdeAlto)
-    contornos, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    for contor in contornos:
-        area = cv2.contourArea(contor) 
-        if area > 3000:
-            centros = cv2.moments(contor)
-            if (centros["m00"] == 0): centros["m00"] = 1
-            x = int(centros["m10"] / centros["m00"])
-            y = int(centros["m01"] / centros["m00"])
-            cv2.circle(frame, (x, y), 7, (9, 231, 9), -1)
-            cv2.putText(frame, '{},{}'.format(x, y), (x + 10, y), font, 0.75, (9, 231, 9), 1, cv2.LINE_AA)
-            contorSuavi = cv2.convexHull(contor) 
-            cv2.drawContours(frame, [contorSuavi], 0, (255, 0, 0), 3) 
+	global coorX, coorY 
+	verdeBajo = np.array([36, 100, 20])
+	verdeAlto = np.array([70, 255, 255])
+	frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	mask = cv2.inRange(frameHSV, verdeBajo, verdeAlto)
+	contornos, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	for contor in contornos:
+		area = cv2.contourArea(contor)
+		if area > 3000:
+			centros = cv2.moments(contor)
+			if (centros["m00"] == 0): centros["m00"] = 1
+			x = int(centros["m10"] / centros["m00"])
+			y = int(centros["m01"] / centros["m00"])
+			cordeX = x
+			cordeY = y 
+			cv2.circle(frame, (x, y), 7, (9, 231, 9), -1)
+			cv2.putText(frame, '{},{}'.format(x, y), (x + 10, y), font, 0.75, (9, 231, 9), 1, cv2.LINE_AA)
+			contorSuavi = cv2.convexHull(contor) 
+			cv2.drawContours(frame, [contorSuavi], 0, (255, 0, 0), 3) 
 
 def openCamera():
-    while (True): 
-        ret, frame = cap.read() 
-        frame = cv2.flip(frame, 1)
-        if ret == True:        
-            pointCoordenates(frame)        
-            cv2.imshow('camara', frame) 
-            if cv2.waitKey(1) & 0xFF == ord('s'): 
-                break
-    cap.release() 
-    cv2.destroyAllWindows() 
-
-
-
-
+	global coorX, coorY, pos_X
+	while (True):
+		ret, frame = cap.read() 
+		frame = cv2.flip(frame, 1)
+		if ret == True:
+			i= frame.shape[0]
+			j= frame.shape[1]
+			x_medio_der = int((j + 60) / 2)
+			x_medio_izq = int((j - 60) / 2)
+			coorX = int(j / 2) 
+			coorY = int(i / 2)
+			cv2.line(frame, (x_medio_der, 0), (x_medio_der, i), (255, 255, 0), 2)
+			cv2.line(frame, (x_medio_izq, 0), (x_medio_izq, i), (255, 255, 0), 2)
+			pointCoordenates(frame)
+			pointCoordenates(frame)
+			cv2.imshow('camara', frame)
+			if (coorX > 0 and coorX < x_medio_izq):
+				pos_x = -0.7
+			if (coorX >= x_medio_izq and coorX <= x_medio_der):
+				pos_x = 0
+			if (coorX > x_medio_der and coorX < j):
+				pos_x = 0.7			
+			if cv2.waitKey(1) & 0xFF == ord('s'):
+				break
+	cap.release() 
+	cv2.destroyAllWindows() 
 
 def car(x,y):  
     gamedisplays.blit(carimg,(x,y))
