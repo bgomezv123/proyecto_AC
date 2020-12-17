d97fbdab898ea06f6df428bfe0930dd69f6882a4
bgomezv123 <bgomezv@unsa.edu.pe>
2020-12-16 20:35:53 -0500

    Movimiento de la imagen(Carro)

--- juego_carreras.py
+++ juego_carreras.py
@@ -25,19 +25,49 @@ gamedisplays=pygame.display.set_mode((display_width,display_height))
 carimg=pygame.image.load('car1.png')
 carimg=pygame.transform.scale(carimg, (56, 125))
 
-def car(x,y):
-    
+def car(x,y):  
     gamedisplays.blit(carimg,(x,y))
 
-while True:
+def juego_loop():
+	
+	x_change = 0
+	x = 400
+	y = 460
 
-    gamedisplays.fill(gray)
+	while True:
+		for event in pygame.event.get():
+			car(400, 300)
+			if event.type == pygame.QUIT:		
+				pygame.quit()
+				quit()
+			if event.type==pygame.KEYDOWN:
+				if event.key==pygame.K_LEFT:
+					x_change=- 1
+				if event.key==pygame.K_RIGHT:
+					x_change = 1
+			if event.type==pygame.KEYUP:
+				if event.key==pygame.K_LEFT or event.key==pygame.K_RIGHT:
+					x_change=0
 
-    car(400, 300)
-    for event in pygame.event.get():
-    	if event.type == pygame.QUIT:
-    	
-    		pygame.quit()
-    		quit()
-    	pygame.display.update()
+		gamedisplays.fill(gray)
 
+		x+=x_change
+		car(x, y)
+
+		pygame.display.update()
+
+
+
+		
+
+
+
+		
+
+
+		
+
+
+
+
+juego_loop()
